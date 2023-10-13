0bb42b8fa53a4b8ad842fa532a44f1807b286088
Hugo Pisano <hugopisano@air-de-hugo.home>
2023-10-13 15:11:22 +0200

    Refactor order status update endpoint and improve SQL security

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -81,81 +81,79 @@ router.put("/:id", function (req, res) {
   });
 });
 
-//UPDATE STATUS BY ID
-router.patch("/:id", function (req, res) {
-  let token = req.headers.authorization.split(" ")[1];
-
-  jwt.verify(token, process.env.JWT_KEY, function (err, decoded) {
-    if (err) res.json("unauthorized");
-
-    const query = `UPDATE orders SET id = ${req.params.id}, status = "${req.body.status}", updatedAt=NOW() WHERE id = ${req.params.id}`;
-
-    if (req.body.status == "validated") {
-      /* 
-      TODO:vérifier qu'il y a bien du stock
-      */
-
-      const query2 = `SELECT * FROM orders WHERE id = ${req.params.id}`;
-
-      req.db.query(query2, (err, rows, fields) => {
-        if (err) console.log(err);
-
-        let user_id = rows[0].user_id;
-
-        const query3 = `SELECT * FROM users WHERE id = ${user_id}`;
-
-        req.db.query(query3, (err, rows, fields) => {
-          if (err) console.log(err);
-
-          const query4 = `UPDATE users SET loyaltyPoints=${loyaltyPoints} WHERE id = ${user_id}`;
-
-          req.db.query(query4, (err, rows, fields) => {
-            if (err) console.log(err);
-
-            res.json("updated");
-          });
-        });
-      });
-      try {
-        let query_PizzasInOrder =
-          "SELECT * FROM orders_pizzas WHERE order_id=" + req.params.id;
-
-        req.db.query(query_PizzasInOrder, (err, rows, fields) => {
-          if (err) throw err;
-
-          rows.forEach((element) => {
-            const queryIngredients = `SELECT * FROM ingredients as ig INNER JOIN pizzas_ingredients as pi ON pi.ingredient_id = ig.id AND pi.pizza_id = ${element.pizza_id};`;
-
-            req.db.query(queryIngredients, (err, rows, fields) => {
-              if (err) console.log(err);
-
-              for (let index = 0; index < rows.length; index++) {
-                const element = rows[index];
-
-                let requete_Mise_A_jour_Stock_Ingredient = `UPDATE ingredients SET stock=${
-                  element.stock - 10
-                } WHERE id = ${element.id}`;
-                req.db.query(
-                  requete_Mise_A_jour_Stock_Ingredient,
-                  (err, rows, fields) => {}
-                );
-                if (err) console.log(err);
-              }
-            });
-          });
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    } else {
-      req.db.query(query, (err, rows, fields) => {
-        if (err) console.log(err);
-
-        res.json("updated");
-      });
-    }
+const queryDB = (db, sql, params) => {
+  return new Promise((resolve, reject) => {
+    db.query(sql, params, (err, results) => {
+      if (err) reject(err);
+      resolve(results);
+    });
   });
+};
+
+//UPDATE STATUS BY ID
+router.patch("/:id", async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
+
+    if (!decoded) {
+      return res.json("unauthorized");
+    }
+
+    const orderId = req.params.id;
+
+    if (req.body.status === "validated") {
+      // TODO: Check if there's enough stock
+      const pizzasInOrder = await queryDB(req.db, "SELECT * FROM orders_pizzas WHERE order_id = ?", [orderId]);
+
+      for (const pizza of pizzasInOrder) {
+        const ingredients = await queryDB(
+          req.db,
+          `
+          SELECT * FROM ingredients as ig 
+          INNER JOIN pizzas_ingredients as pi 
+          ON pi.ingredient_id = ig.id AND pi.pizza_id = ?;
+        `,
+          [pizza.pizza_id]
+        );
+
+        for (const ingredient of ingredients) {
+          await queryDB(
+            req.db,
+            `
+            UPDATE ingredients 
+            SET stock = ? 
+            WHERE id = ?;
+          `,
+            [ingredient.stock - 10, ingredient.id]
+          );
+        }
+      }
+
+      const orders = await queryDB(req.db, "SELECT * FROM orders WHERE id = ?", [orderId]);
+      const userId = orders[0].user_id;
+
+      const loyaltyPoints = 10; // This is just a placeholder. You should define how you calculate this.
+      await queryDB(req.db, "UPDATE users SET loyaltyPoints = ? WHERE id = ?", [loyaltyPoints, userId]);
+    }
+
+    await queryDB(
+      req.db,
+      `
+      UPDATE orders 
+      SET id = ?, status = ?, updatedAt = NOW() 
+      WHERE id = ?;
+    `,
+      [orderId, req.body.status, orderId]
+    );
+
+    res.json("updated");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json("Internal Server Error");
+  }
 });
+
 
 //READ ONE BY ID
 router.get("/:id", function (req, res) {
